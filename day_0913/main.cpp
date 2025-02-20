a6602b4bc7f574d07f69771d203e4f3e9005d246
YW8862 <2165628227@qq.com>
2025-02-20 20:26:07 +0800

    刷题更新

--- day_0913/main.cpp
+++ day_0913/main.cpp
@@ -2888,3 +2888,17 @@ private:
 // };
 
 
+//leetcode 89
+// class Solution
+// {
+// public:
+//     vector<int> grayCode(int n)
+//     {
+//         vector<int>ans(1<<n);
+//         for(int i=0;i<ans.size();i++)
+//         {
+//             ans[i] = i^(i/2);
+//         }
+//         return ans;
+//     }
+// };
