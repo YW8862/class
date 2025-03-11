3e97d3565389728ebeb06e69dec28e58093c398f
YW8862 <2165628227@qq.com>
2025-03-11 22:27:48 +0800

    刷题更新

--- Exercise/main.cpp
+++ Exercise/main.cpp
@@ -4184,3 +4184,23 @@ public:
 // 		return ans;
 // 	}
 // };
+
+
+//乐儿他从的122
+//法一:贪心算法，有利就贪
+// class Solution
+// {
+// public:
+//     int maxProfit(vector<int>& prices)
+//     {
+//         int profit = 0;
+//         for(int i=1;i<prices.size();i++)
+//         {
+//             if(prices[i] > prices[i-1])
+//             {
+//                 profit += (prices[i] - prices[i-1]);
+//             }
+//         }
+//         return profit;
+//     }
+// };
